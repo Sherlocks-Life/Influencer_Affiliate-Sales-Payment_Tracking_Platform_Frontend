500930f7116024ab4ea11170fdc0d30b4cbf24d3
AKHIL THADAKA <akhilthadaka97@gmail.com>
2026-05-07 11:21:08 +0530

    Create socket.js

---
+++ src/socket.js
@@ -0,0 +1,19 @@
+import { io } from "socket.io-client";
+
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL ||
+  "https://influencer-affiliate-sales-payment-o2l2.onrender.com";
+
+export const socket = io(SOCKET_URL, {
+  transports: ["websocket"],
+
+  withCredentials: true,
+
+  autoConnect: true,
+
+  reconnection: true,
+
+  reconnectionAttempts: 5,
+
+  reconnectionDelay: 2000,
+});
